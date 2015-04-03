dd30633c8598f0224579a398ede365552b66ec32
Ryan Bennick <ryan@320ny.com>
2015-04-03 10:54:58 -0600

    Started sync code

--- dualaudio.js
+++ dualaudio.js
@@ -5,57 +5,94 @@
 //    2. The dom id of the audio file
 //    3. The dom id of the button
 //    4. The offset of the start of the second audio track
-//    5. The button label for audio track 1
-//    6. The button label for audio track 2
 //
 
 (function() {
   var Beneful = function () {
+    this.currentAudioTrack = 1;
+    this.audioElm = undefined;
+    this.audioBtn = undefined;
+    this.iframeName = undefined;
+
+    
     this.dualAudioOneFile = function(iframeName, audioId, buttonId, offset) {
-      var _this = this;
+      
       var iframeName;
       var audioId;
       var buttonId;
       var offset;
       
-      var audioElm = document.getElementById(audioId);
-      var audioBtn = document.getElementById(buttonId);
-
+      this.iframeName = iframeName;
+      this.audioElm = document.getElementById(audioId);
+      this.audioBtn = document.getElementById(buttonId);
+      
+      this._registerRaptorEvents();
+      this._setUpButtonEventListener(this.iframeName, offset);
+      this._syncVideoAudioWatcher(500);
+    };   
+      
+    this._registerRaptorEvents = function() {
+      var _this = this;
       raptor.api.on("ready", function(event, el){
         raptor.settings("defaultIFrame", el.name);
-       });
+      });
 
       raptor.api.on("projectStart", function(){
-      	audioElm.load();
-      	audioElm.currentTime = 0;
-      	audioElm.play();
+        _this.audioElm.load();
+        _this.audioElm.currentTime = 0;
+     	_this.audioElm.play();
       });
 
       raptor.api.on("pause", function(){
-      	audioElm.pause();
+	    _this.audioElm.pause();
       });
 
       raptor.api.on("play", function(){
-      	audioElm.play();
+        _this.audioElm.play();
       });
-      
-      raptor.api.on("progress", function(){
-        console.log("Progress");
-      });
-
-      audioBtn.addEventListener('click', function() {
-         videoProgress = (raptor.api.state(iframeName).progressTime);
-         if(_this.currentAudioTrack == 1) {
-           audioElm.currentTime = videoProgress + offset;
-           _this.currentAudioTrack = 2;
-         } else {
-           audioElm.currentTime = videoProgress
-           _this.currentAudioTrack = 1;
-         }
-      });
-      
     };
-    this.currentAudioTrack = 1;
+    
+    this._setUpButtonEventListener = function(iframeName, offset) {
+      var _this = this;
+      this.audioBtn.addEventListener('click', function() {
+        videoProgress = (raptor.api.state(iframeName).progressTime);
+        if(_this.currentAudioTrack == 1) {
+          _this.audioElm.currentTime = videoProgress + offset;
+          _this.currentAudioTrack = 2;
+        } else {
+          _this.audioElm.currentTime = videoProgress
+          _this.currentAudioTrack = 1;
+        }
+      });
+    };
+    
+    this._syncVideoAudioWatcher = function(inverval) {
+      var inverval = inverval;
+      var intervalId = undefined;
+      var _this = this;
+      raptor.api.on("projectStart", function(){
+        intervalId = setInterval(function() {
+          if (_this._videoAutoOutofSync()) {
+            _this._synchVideoAudio();
+          }
+        }, inverval);
+      }); 
+      raptor.api.on("projectEnd", function(){
+          clearInterval(intervalId);
+      });
+    };
+    
+    this._videoAutoOutofSync = function() {
+      var videoProgressTime = raptor.api.state(this.iframeName).progressTime;
+      var audioProgressTime = this.audioElm.currentTime;
+      console.log('Video Progress :' + videoProgressTime);
+      console.log('Audio Progress :' + audioProgressTime);
+    };
+    
+    this._synchVideoAudio = function() {
+      console.log("Syncing video and audio!!!!!");
+    };
+    
   }
   window.Beneful = Beneful;
 })();
